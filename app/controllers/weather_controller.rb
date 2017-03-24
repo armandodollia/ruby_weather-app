0088595a1b05fe967606c270d233ba0a355a8a3b
Armando Dollia <dollia.armando@gmail.com>
2017-03-24 11:44:57 -0500

    Implements showing weather

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -1,9 +1,10 @@
-get '/weather/new' do
-  
-  erb :'weather/new'
+get '/weather/show' do
+  @current_weather = get_weather(params[:city_name], params[:unit_type])
+  p @current_weather
+  erb :'weather/index'
 end
 
 get '/weather' do
-  @current_weather = get_weather
+  # @current_weather = get_weather
   erb :'weather/index'
 end
--- app/helpers/openweather_api_helper.rb
+++ app/helpers/openweather_api_helper.rb
@@ -4,10 +4,18 @@ helpers do
   end
   
   def get_weather(city='Chicago', units='imperial')
-    JSON.load(open("http://api.openweathermap.org/data/2.5/weather?q=Chicago&APPID=#{weather_api_key}&units=#{units}"))
+    JSON.load(open("http://api.openweathermap.org/data/2.5/weather?q=#{city}&APPID=#{weather_api_key}&units=#{units}"))
   end
   
   def get_icon_source(icon_code)
     "http://openweathermap.org/img/w/#{icon_code}.png"
+  end
+  
+  def temperature(weather)
+    weather['main']['temp']
+  end
+  
+  def pressure(weather)
+    weather['main']['pressure']
   end
 end
